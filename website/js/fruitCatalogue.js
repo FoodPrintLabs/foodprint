f780175030f205381261fb5b0f81d12ee16dcfce
Thandeka <ttrchehore@gmail.com>
2019-07-22 20:43:27 +0200

    Added fruit json db

---
+++ website/js/fruitCatalogue.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+      id: 1,
+      pic:
+        "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/2014/03/13-flat-twist-with-twist-out-2.jpg?w=500&ssl=1",
+      name: "Flat-Twist With Twist Out",
+      difficulty: "Medium",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/"
+    },
+    {
+      id: 2,
+      pic:
+        "https://i1.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/4-natural-short-ponytail-with-twists.jpg?w=476&ssl=1",
+      name: "Updo With Bangs for Short Hair",
+      difficulty: "Easy",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/7/"
+    },
+    {
+      id: 3,
+      pic:
+        "https://i0.wp.com/therighthairstyles.com/wp-content/uploads/1500x/short-natural-hairstyles/2-updo-with-didi-braids.jpg?w=486&ssl=1",
+      name: "Inverted Cornrows",
+      difficulty: "Hard",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/12/"
+    }
+  ];
