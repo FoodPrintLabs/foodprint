4ca376c29fe3dbfe3a7a451b0f9be4b884ae04b4
juliank <julian.kanjere@jskdezign.com>
2019-11-17 18:58:06 +0200

    Update to use SQL connection pooling

--- src/js/db.js
+++ src/js/db.js
@@ -1,11 +1,13 @@
 var mysql=require('mysql');
 var config  = require("../../dbconfig");
-var connection=mysql.createConnection(config.db);
-connection.connect(function(error){
-    if(error){
-         console.log(error);
-        }else{
-         console.log('Connected!:)');
-        }
-    });
-module.exports = connection; 
+//var connection=mysql.createConnection(config.db);
+var pool  = mysql.createPool(config.db_pool);
+// connection.connect(function(error){
+//     if(error){
+//          console.log(error);
+//         }else{
+//          console.log('Connected!:)');
+//         }
+//     });
+// module.exports = connection;
+module.exports = pool;
