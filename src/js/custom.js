081a8e6a132e58f97f1f2db7ffcd50d6e61ab6ee
juliank <julian.kanjere@jskdezign.com>
2019-12-12 00:02:04 +0200

    Update js for footer partial subscription form

--- src/js/custom.js
+++ src/js/custom.js
@@ -3,4 +3,36 @@ jQuery(document).ready(function() {
 	"use strict";
 	// Your custom js code goes here.
 
+	$('#subscribe_form').submit(function (event) {
+		event.preventDefault();
+		//collect the form data using Id Selector what ever data you need to send to server
+		var data = {};
+		data.subscribe_email=$('#subscribe_email').val();
+		$.ajax({
+			url: '/subscribe',
+			data: JSON.stringify(data),
+			processData: false,
+			type: 'POST',
+			contentType: 'application/json'
+		}).done(function(res) {
+			if (res.success) {
+				//$("#subscribe_form").get(0).reset() // or $('form')[0].reset()
+				$("#subscribe_form").hide();
+				$('#error-group').hide();
+				$('#subscribe_result').html('You have successfully subscribed as ' + res.email);
+			} else {
+				$('#error-group').css('display', 'block');
+				  var errors = JSON.parse(JSON.stringify(res.errors));
+				  var errorsContainer = $('#errors');
+				  errorsContainer.innerHTML = '';
+				  var errorsList = '';
+
+				  for (var i = 0; i < errors.length; i++) {
+					errorsList += '<li>' + errors[i].msg + '</li>';
+				  }
+				  errorsContainer.html(errorsList);
+				  console.log('error...ajax' + errorsList);
+			}
+		  });
+	});
 });
