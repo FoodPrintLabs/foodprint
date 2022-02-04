8c749f2b526c3b63b06fcbcd5091c5a785cceae7
Daniel Aaron <danielaaron7@gmail.com>
2022-02-04 15:41:40 +0200

    Updated model inc farm,intermed and agent values

--- models/user.js
+++ models/user.js
@@ -1,84 +1,98 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('User', {
-    ID: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    "User",
+    {
+      ID: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      firstName: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      middleName: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      lastName: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        unique: "email",
+      },
+      phoneNumber: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        unique: "phoneNumber",
+      },
+      role: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      password: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      registrationChannel: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      nationalIdPhotoHash: {
+        type: DataTypes.BLOB,
+        allowNull: true,
+      },
+      farmName: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      organisationName: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      organisationType: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      city: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
     },
-    firstName: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    middleName: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    lastName: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    email: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: "email"
-    },
-    phoneNumber: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: "phoneNumber"
-    },
-    role: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    password: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    registrationChannel: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    nationalIdPhotoHash: {
-      type: DataTypes.BLOB,
-      allowNull: true
+    {
+      sequelize,
+      tableName: "user",
+      timestamps: false,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "ID" }],
+        },
+        {
+          name: "email",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "email" }],
+        },
+        {
+          name: "phoneNumber",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "phoneNumber" }],
+        },
+      ],
     }
-  }, {
-    sequelize,
-    tableName: 'user',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "ID" },
-        ]
-      },
-      {
-        name: "email",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "email" },
-        ]
-      },
-      {
-        name: "phoneNumber",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "phoneNumber" },
-        ]
-      },
-    ]
-  });
+  );
 };
