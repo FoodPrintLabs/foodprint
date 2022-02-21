15350e7616aa3af736cf211b9b284bb2fd43d8b5
Julian Kanjere <julian.kanjere@jskdezign.com>
2022-02-21 22:18:27 +0200

    add legacy supplychain route

--- routes/legacy.js
+++ routes/legacy.js
@@ -1,82 +1,73 @@
 var express = require('express');
 var router = express.Router();
-var connection = require("../src/js/db");
+var connection = require('../src/js/db');
 
 //return template with scan results for produce
 //NB this is an old template (scanresultv1) which probably should be removed
 router.get('/scan/:id', function (req, res) {
   var supplierProduceID = req.params.id; //OranjezichtCityFarm_Apples
-  var boolTracedOnBlockchain = process.env.SHOW_TRACED_ON_BLOCKCHAIN || false
+  var boolTracedOnBlockchain = process.env.SHOW_TRACED_ON_BLOCKCHAIN || false;
 
   // http://localhost:3000/scan/OranjezichtCityFarm_Apples
-  connection.execute('\n' +
-    'SELECT \n' +
-    '\ts.counter,\n' +
-    '\ts.ID,\n' +
-    '\ts.marketID,\n' +
-    '\ts.marketAddress,\n' +
-    '\ts.quantity,\n' +
-    '\ts.unitOfMeasure,\n' +
-    '\ts.storageTimeStamp,\n' +
-    '\ts.storageCaptureTime,\n' +
-    '\ts.URL,\n' +
-    '\ts.hashID,\n' +
-    '\ts.storageDescription,\n' +
-    '\ts.geolocation,\n' +
-    '\ts.supplierproduce,\n' +
-    '\th.supplierID,\n' +
-    '\th.supplierAddress,\n' +
-    '\th.productID,\n' +
-    '\th.photoHash,\n' +
-    '\th.harvestTimeStamp,\n' +
-    '\th.harvestCaptureTime,\n' +
-    '\th.harvestDescription,\n' +
-    '\th.geolocation\n' +
-    'FROM \n' +
-    '\tstorage s \n' +
-    'INNER JOIN\n' +
-    '\tharvest h\n' +
-    'ON \n' +
-    '\ts.supplierproduce = h.supplierproduce\n' +
-    'WHERE  \n' +
-    '\ts.supplierproduce = ? AND \n' +
-    '\th.counter = (SELECT max(counter) FROM harvest where supplierproduce = ?) AND \n' +
-    '    s.counter = (SELECT max(counter) FROM storage where supplierproduce = ?);',
-    [
-      supplierProduceID,
-      supplierProduceID,
-      supplierProduceID
-    ],
+  connection.execute(
+    '\n' +
+      'SELECT \n' +
+      '\ts.counter,\n' +
+      '\ts.ID,\n' +
+      '\ts.marketID,\n' +
+      '\ts.marketAddress,\n' +
+      '\ts.quantity,\n' +
+      '\ts.unitOfMeasure,\n' +
+      '\ts.storageTimeStamp,\n' +
+      '\ts.storageCaptureTime,\n' +
+      '\ts.URL,\n' +
+      '\ts.hashID,\n' +
+      '\ts.storageDescription,\n' +
+      '\ts.geolocation,\n' +
+      '\ts.supplierproduce,\n' +
+      '\th.supplierID,\n' +
+      '\th.supplierAddress,\n' +
+      '\th.productID,\n' +
+      '\th.photoHash,\n' +
+      '\th.harvestTimeStamp,\n' +
+      '\th.harvestCaptureTime,\n' +
+      '\th.harvestDescription,\n' +
+      '\th.geolocation\n' +
+      'FROM \n' +
+      '\tstorage s \n' +
+      'INNER JOIN\n' +
+      '\tharvest h\n' +
+      'ON \n' +
+      '\ts.supplierproduce = h.supplierproduce\n' +
+      'WHERE  \n' +
+      '\ts.supplierproduce = ? AND \n' +
+      '\th.counter = (SELECT max(counter) FROM harvest where supplierproduce = ?) AND \n' +
+      '    s.counter = (SELECT max(counter) FROM storage where supplierproduce = ?);',
+    [supplierProduceID, supplierProduceID, supplierProduceID],
     function (err, rows) {
       if (err) {
         //req.flash('error', err);
         console.error('error', err);
         res.render('scanresultv1', {
-          data: '', user: req.user,
+          data: '',
+          user: req.user,
           showTracedOnBlockchain: boolTracedOnBlockchain,
-          page_name: 'scanresultv1'
+          page_name: 'scanresultv1',
         });
       } else {
         res.render('scanresultv1', {
-          data: rows, user: req.user,
+          data: rows,
+          user: req.user,
           showTracedOnBlockchain: boolTracedOnBlockchain,
-          page_name: 'scanresultv1'
+          page_name: 'scanresultv1',
         });
       }
-    });
+    }
+  );
 });
-
-
-
 
 //TODO Add Weekly View route and template
 //TODO Add Weekly View REST API
-
-
-
-
-
-
 
 //addHarvest XmlHTTP request
 router.post('/app/addHarvest', function (req, res) {
@@ -84,19 +75,20 @@ router.post('/app/addHarvest', function (req, res) {
   // geolocation,supplierproduce
   // console.log("addHarvest" + req.body);
   try {
-    connection.query('\n' +
-      'INSERT INTO harvest (\n' +
-      '        ID ,\n' +
-      '        supplierID,\n' +
-      '        supplierAddress,\n' +
-      '        productID,\n' +
-      '        photoHash,\n' +
-      '        harvestTimeStamp,\n' +
-      '        harvestCaptureTime,\n' +
-      '        harvestDescription,\n' +
-      '        geolocation,\n' +
-      '        supplierproduce)\n' +
-      'VALUES (?, ?,?,?,?,?,?,?,?, ?);',
+    connection.query(
+      '\n' +
+        'INSERT INTO harvest (\n' +
+        '        ID ,\n' +
+        '        supplierID,\n' +
+        '        supplierAddress,\n' +
+        '        productID,\n' +
+        '        photoHash,\n' +
+        '        harvestTimeStamp,\n' +
+        '        harvestCaptureTime,\n' +
+        '        harvestDescription,\n' +
+        '        geolocation,\n' +
+        '        supplierproduce)\n' +
+        'VALUES (?, ?,?,?,?,?,?,?,?, ?);',
       [
         req.body.ID,
         req.body.supplierID,
@@ -107,21 +99,22 @@ router.post('/app/addHarvest', function (req, res) {
         req.body.harvestCaptureTime,
         req.body.harvestDescription,
         req.body.geolocation,
-        req.body.supplierproduce
-      ], function (err, rows) {
+        req.body.supplierproduce,
+      ],
+      function (err, rows) {
         if (err) {
           //req.flash('error', err);
           console.error('error', err);
         } else {
           console.log('addHarvest DB success');
         }
-      });
+      }
+    );
   } catch (e) {
     //this will eventually be handled by your error handling middleware
-    next(e)
+    next(e);
   }
 });
-
 
 //addStorage XmlHTTP request
 router.post('/app/addStorage', function (req, res) {
@@ -130,20 +123,21 @@ router.post('/app/addStorage', function (req, res) {
 
   // console.log("addStorage" + req.body);
   try {
-    connection.query('INSERT INTO storage (\n' +
-      '        ID,\n' +
-      '        marketID,\n' +
-      '        marketAddress,\n' +
-      '        quantity,\n' +
-      '        unitOfMeasure,\n' +
-      '        storageTimeStamp,\n' +
-      '        storageCaptureTime,\n' +
-      '        URL,\n' +
-      '        hashID,\n' +
-      '        storageDescription,\n' +
-      '        geolocation,\n' +
-      '        supplierproduce)\n' +
-      'VALUES (?, ?,?,?, ?,?, ?,?, ?, ?, ?, ?);',
+    connection.query(
+      'INSERT INTO storage (\n' +
+        '        ID,\n' +
+        '        marketID,\n' +
+        '        marketAddress,\n' +
+        '        quantity,\n' +
+        '        unitOfMeasure,\n' +
+        '        storageTimeStamp,\n' +
+        '        storageCaptureTime,\n' +
+        '        URL,\n' +
+        '        hashID,\n' +
+        '        storageDescription,\n' +
+        '        geolocation,\n' +
+        '        supplierproduce)\n' +
+        'VALUES (?, ?,?,?, ?,?, ?,?, ?, ?, ?, ?);',
       [
         req.body.ID,
         req.body.marketID,
@@ -156,18 +150,31 @@ router.post('/app/addStorage', function (req, res) {
         req.body.hashID,
         req.body.storageDescription,
         req.body.geolocation,
-        req.body.supplierproduce
-      ], function (err, rows) {
+        req.body.supplierproduce,
+      ],
+      function (err, rows) {
         if (err) {
           console.error('error', err);
         } else {
           console.log('addStorage DB success');
         }
-      });
+      }
+    );
   } catch (e) {
     //this will eventually be handled by your error handling middleware
-    next(e)
+    next(e);
   }
 });
+
+//supply chain - harvest and storage
+router.get(
+  '/app/supplychain',
+  require('connect-ensure-login').ensureLoggedIn({
+    redirectTo: '/app/auth/login',
+  }),
+  function (req, res) {
+    res.render('supplychain', { user: req.user, page_name: 'supplychain' });
+  }
+);
 
 module.exports = router;
