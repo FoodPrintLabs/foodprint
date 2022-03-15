9fffbc47b2e80de9d484c5c579d08f5d0d3737a1
Daniel Aaron <danielaaron7@gmail.com>
2022-03-15 10:50:35 +0200

    HarvestData Table for Admin and Farmer(Unfinished)

--- routes/dashboards.js
+++ routes/dashboards.js
@@ -1,7 +1,134 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
+const uuidv4 = require('uuid/v4');
+var body = require('express-validator'); //validation
 
-/*Render admin dashboard */
+var connection = require('../src/js/db');
+var ROLES = require('../utils/roles');
+var fs = require('fs');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+var initModels = require('../models/init-models');
+var sequelise = require('../src/js/db_sequelise');
+
+var models = initModels(sequelise);
+
+router.get(
+  '/admin',
+  require('connect-ensure-login').ensureLoggedIn({ redirectTo: '/app/auth/login' }),
+  function (req, res, next) {
+    if (req.user.role === ROLES.Admin) {
+      models.FoodprintHarvest.findAll({
+        order: [['pk', 'DESC']],
+      })
+        .then(rows => {
+          for (let i = 0; i < rows.length; i++) {
+            rows[i].harvest_photoHash =
+              'data:image/png;base64,' +
+              new Buffer(rows[i].harvest_photoHash, 'binary').toString('base64');
+          }
+          res.render('dashboard_admin', {
+            title: 'FoodPrint - Admin Dashboard',
+            data: rows,
+            user: req.user,
+            page_name: 'Dashboard',
+          });
+        })
+        .catch(err => {
+          req.flash('error', err);
+          res.render('harvestlogbook', {
+            page_title: 'FoodPrint - Harvest Logbook',
+            data: '',
+            user: req.user,
+            page_name: 'harvestlogbook',
+          });
+        });
+      //   connection.execute('SELECT * FROM foodprint_harvest ORDER BY pk desc',function(err,rows)     {
+      //       if(err){
+      //            req.flash('error', err);
+      //            res.render('harvestlogbook',{  page_title:"FoodPrint - Harvest Logbook",
+      //                                   data:'', user: req.user, page_name:'harvestlogbook' });
+      //       }else{
+      //           for (i=0; i<rows.length; i++)
+      //           {
+      //               rows[i].harvest_photoHash = 'data:image/png;base64,' +
+      //                   new Buffer(rows[i].harvest_photoHash, 'binary').toString('base64');
+      //           }
+      //           res.render('harvestlogbook',{   page_title:"FoodPrint - Harvest Logbook",
+      //                                   data:rows, user: req.user,
+      //                                   page_name:'harvestlogbook' });
+      //       }
+      //    });
+    } else {
+      res.render('error', {
+        message: 'You are not authorised to view this resource.',
+        title: 'Error',
+        user: req.user,
+        page_name: 'error',
+      });
+    }
+  }
+);
+
+router.get(
+  '/farmer',
+  require('connect-ensure-login').ensureLoggedIn({ redirectTo: '/app/auth/login' }),
+  function (req, res, next) {
+    if (req.user.role === ROLES.Farmer) {
+      models.FoodprintHarvest.findAll({
+        order: [['pk', 'DESC']],
+      })
+        .then(rows => {
+          for (let i = 0; i < rows.length; i++) {
+            rows[i].harvest_photoHash =
+              'data:image/png;base64,' +
+              new Buffer(rows[i].harvest_photoHash, 'binary').toString('base64');
+          }
+          res.render('dashboard_farmer', {
+            title: 'FoodPrint - Farmer Dashboard',
+            data: rows,
+            user: req.user,
+            page_name: 'Dashboard',
+          });
+        })
+        .catch(err => {
+          req.flash('error', err);
+          res.render('harvestlogbook', {
+            page_title: 'FoodPrint - Harvest Logbook',
+            data: '',
+            user: req.user,
+            page_name: 'harvestlogbook',
+          });
+        });
+      //   connection.execute('SELECT * FROM foodprint_harvest ORDER BY pk desc',function(err,rows)     {
+      //       if(err){
+      //            req.flash('error', err);
+      //            res.render('harvestlogbook',{  page_title:"FoodPrint - Harvest Logbook",
+      //                                   data:'', user: req.user, page_name:'harvestlogbook' });
+      //       }else{
+      //           for (i=0; i<rows.length; i++)
+      //           {
+      //               rows[i].harvest_photoHash = 'data:image/png;base64,' +
+      //                   new Buffer(rows[i].harvest_photoHash, 'binary').toString('base64');
+      //           }
+      //           res.render('harvestlogbook',{   page_title:"FoodPrint - Harvest Logbook",
+      //                                   data:rows, user: req.user,
+      //                                   page_name:'harvestlogbook' });
+      //       }
+      //    });
+    } else {
+      res.render('error', {
+        message: 'You are not authorised to view this resource.',
+        title: 'Error',
+        user: req.user,
+        page_name: 'error',
+      });
+    }
+  }
+);
+
+/*Render admin dashboard 
 router.get('/admin', function (req, res) {
   res.render('dashboard_admin', {
     title: 'FoodPrint - Admin Dashboard',
@@ -10,7 +137,7 @@ router.get('/admin', function (req, res) {
   });
 });
 
-/*Render farmer dashboard */
+/*Render farmer dashboard 
 router.get('/farmer', function (req, res) {
   res.render('dashboard_farmer', {
     title: 'FoodPrint - Farmer Dashboard',
@@ -18,6 +145,7 @@ router.get('/farmer', function (req, res) {
     page_name: 'Dashboard',
   });
 });
+*/
 
 //TODO
 //GET ALL CROPS HARVESTED
