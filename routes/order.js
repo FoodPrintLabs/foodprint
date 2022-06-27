50a8530e19903ee36b87dc3762dc9383c0e85a66
DanielMAaron <danielaaron7@gmail.com>
2022-06-27 22:38:33 +0200

    customEmail func and email templating

--- routes/order.js
+++ routes/order.js
@@ -669,27 +669,45 @@ router.post(
               where: { bid_logid: req.body.bid_logid },
             }
           )
-          .then(
-            //FOR TESTING EMAILS
-            customSendEmail(
-              'daniela@foodprintlabs.com',
-              'You Accepted a Bid',
-              'Thank you ' +
-                req.user.email +
-                ' for accepting a bid for ' +
-                req.body.bid_produceName +
-                ' from user - ' +
-                req.body.bid_user
-            ),
-            customSendEmail(
-              'daniela@foodprintlabs.com',
-              'Bid Accepted',
-              'Hello your bid of ' +
-                req.body.bid_produceName +
-                ' was accepted from from user - ' +
-                req.user.email
+            .then(
+              //FOR TESTING EMAILS
+              customSendEmail(
+                'daniela@foodprintlabs.com',
+                'You Accepted a Bid',
+                acceptOrderEmail(
+                  req.body.bid_produceName,
+                  req.body.bid_quantity,
+                  req.body.bid_user,
+                  req.user.email,
+                  req.body.bid_logid,
+                  uuidv4(),
+                  'Bid',
+                  req.body.bid_timeStamp,
+                  moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+                  req.body.bid_price,
+                  req.body.bid_province,
+                  'Seller'
+                )
+              ),
+              customSendEmail(
+                'daniela@foodprintlabs.com',
+                'Your bid was accepted',
+                acceptOrderEmail(
+                  req.body.bid_produceName,
+                  req.body.bid_quantity,
+                  req.body.bid_user,
+                  req.user.email,
+                  req.body.bid_logid,
+                  uuidv4(),
+                  'Bid',
+                  req.body.bid_timeStamp,
+                  moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+                  req.body.bid_price,
+                  req.body.bid_province,
+                  'Accepted'
+                )
+              )
             )
-          )
             .then(_ => {
               req.flash(
                 'success',
@@ -771,23 +789,44 @@ router.post(
           )
             .then(
               //FOR TESTING EMAILS
+              //EMAIL TO BUYER
               customSendEmail(
                 'daniela@foodprintlabs.com',
-                'You Accepted an offer',
-                'Thank you ' +
-                  req.user.email +
-                  ' for accepting an offer for ' +
-                  req.body.offer_produceName +
-                  ' from user - ' +
-                  req.body.offer_user
+                'You accepted an offer',
+                //body for email
+                acceptOrderEmail(
+                  req.body.offer_produceName,
+                  req.body.offer_quantity,
+                  req.body.offer_user,
+                  req.user.email,
+                  req.body.offer_logid,
+                  uuidv4(),
+                  'Offer',
+                  req.body.offer_timeStamp,
+                  moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+                  req.body.offer_price,
+                  req.body.offer_province,
+                  'Buyer'
+                )
               ),
+              //EMAIL TO SELLER
               customSendEmail(
                 'daniela@foodprintlabs.com',
-                'Offer Accepted',
-                'Hello your offer of ' +
-                  req.body.offer_produceName +
-                  ' was accepted from from user - ' +
-                  req.user.email
+                'Your offer was accepted',
+                acceptOrderEmail(
+                  req.body.offer_produceName,
+                  req.body.offer_quantity,
+                  req.body.offer_user,
+                  req.user.email,
+                  req.body.offer_logid,
+                  uuidv4(),
+                  'Offer',
+                  req.body.offer_timeStamp,
+                  moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+                  req.body.offer_price,
+                  req.body.offer_province,
+                  'Accepted'
+                )
               )
             )
             .then(_ => {
@@ -823,5 +862,72 @@ router.post(
     }
   }
 );
+
+function acceptOrderEmail(
+  produceName,
+  quantity,
+  order_user,
+  current,
+  original_logid,
+  logid,
+  type,
+  original_timeStamp,
+  timeStamp,
+  price,
+  province,
+  to
+) {
+  //email for buyer accepting
+  if (to == 'Buyer') {
+    return `<h3>Offer accepted!</h3>
+            <p>Hello ${current}! You accepted an order of ${produceName}. Listed below is your order </p>
+             <table style="border: 1px solid black; border-collapse: collapse;">
+            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Produce Name </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${produceName}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Province </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${province}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Quantity </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${quantity}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Price </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${price}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Time Accepted </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${timeStamp}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>From </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${order_user}</td></tr>
+            </table>
+            <p><b>Order ID</b> - ${logid} </p>
+            <p><b>Offer ID</b> - ${original_logid} <b>Time Placed</b> - ${original_timeStamp} </p>
+          
+    `;
+  }
+  //email for seller accepting
+  else if (to == 'Seller') {
+    return `<h3>Bid accepted!</h3>
+    <p>Hello ${current}! You accepted an order of ${produceName}. Listed below is your order </p>
+     <table style="border: 1px solid black; border-collapse: collapse;">
+    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Produce Name </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${produceName}</td></tr>
+    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Province </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${province}</td></tr>
+    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Quantity </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${quantity}</td></tr>
+    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Price </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${price}</td></tr>
+    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Time Accepted </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${timeStamp}</td></tr>
+    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>From </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${order_user}</td></tr>
+    </table>
+    <p><b>Order ID</b> - ${logid} </p>
+    <p><b>Bid ID</b> - ${original_logid} <b>Time Placed</b> - ${original_timeStamp} </p>
+  
+`;
+  }
+  //General receiving email for your order being accepted
+  else if (to == 'Accepted') {
+    return `<h3>Order Accepted!</h3>
+            <p>Hello ${current}! Your ${type} of ${produceName} was accepted by ${order_user}. Listed below is your accepted order </p>
+            <table style="border: 1px solid black; border-collapse: collapse;">
+            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Produce Name </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${produceName}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>Province </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${province}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>Quantity </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${quantity}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>Price </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${price}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>Time Accepted </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${timeStamp}</td></tr>
+            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>From </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${current}</td></tr>
+            </table>
+            <p><b>Order ID</b> - ${logid} </p>
+            <p><b>Offer ID</b> - ${original_logid} <b>Time Placed</b> - ${original_timeStamp} </p>
+          
+    `;
+  }
+}
 
 module.exports = router;
