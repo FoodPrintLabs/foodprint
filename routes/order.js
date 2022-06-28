c3218b516d5f295386022c0240a567646c23532c
DanielMAaron <danielaaron7@gmail.com>
2022-06-28 18:23:24 +0200

    orderEmailTemplate moved and imported

--- routes/order.js
+++ routes/order.js
@@ -11,6 +11,7 @@ var fs = require('fs');
 const axios = require('axios');
 const crypto = require('crypto');
 const hash = crypto.createHash('sha256');
+let acceptOrderEmail = require('../config/email/orderEmailTemplate');
 
 //email functionality
 let customSendEmail = require('../config/email/email');
@@ -672,7 +673,7 @@ router.post(
             .then(
               //FOR TESTING EMAILS
               customSendEmail(
-                'daniela@foodprintlabs.com',
+                req.user.email,
                 'You Accepted a Bid',
                 acceptOrderEmail(
                   req.body.bid_produceName,
@@ -690,7 +691,7 @@ router.post(
                 )
               ),
               customSendEmail(
-                'daniela@foodprintlabs.com',
+                req.body.bid_user,
                 'Your bid was accepted',
                 acceptOrderEmail(
                   req.body.bid_produceName,
@@ -791,7 +792,7 @@ router.post(
               //FOR TESTING EMAILS
               //EMAIL TO BUYER
               customSendEmail(
-                'daniela@foodprintlabs.com',
+                req.user.email,
                 'You accepted an offer',
                 //body for email
                 acceptOrderEmail(
@@ -811,7 +812,7 @@ router.post(
               ),
               //EMAIL TO SELLER
               customSendEmail(
-                'daniela@foodprintlabs.com',
+                req.body.offer_user,
                 'Your offer was accepted',
                 acceptOrderEmail(
                   req.body.offer_produceName,
@@ -862,72 +863,5 @@ router.post(
     }
   }
 );
-
-function acceptOrderEmail(
-  produceName,
-  quantity,
-  order_user,
-  current,
-  original_logid,
-  logid,
-  type,
-  original_timeStamp,
-  timeStamp,
-  price,
-  province,
-  to
-) {
-  //email for buyer accepting
-  if (to == 'Buyer') {
-    return `<h3>Offer accepted!</h3>
-            <p>Hello ${current}! You accepted an order of ${produceName}. Listed below is your order </p>
-             <table style="border: 1px solid black; border-collapse: collapse;">
-            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Produce Name </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${produceName}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Province </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${province}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Quantity </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${quantity}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Price </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${price}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Time Accepted </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${timeStamp}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>From </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${order_user}</td></tr>
-            </table>
-            <p><b>Order ID</b> - ${logid} </p>
-            <p><b>Offer ID</b> - ${original_logid} <b>Time Placed</b> - ${original_timeStamp} </p>
-          
-    `;
-  }
-  //email for seller accepting
-  else if (to == 'Seller') {
-    return `<h3>Bid accepted!</h3>
-    <p>Hello ${current}! You accepted an order of ${produceName}. Listed below is your order </p>
-     <table style="border: 1px solid black; border-collapse: collapse;">
-    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Produce Name </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${produceName}</td></tr>
-    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Province </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${province}</td></tr>
-    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Quantity </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${quantity}</td></tr>
-    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Price </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${price}</td></tr>
-    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Time Accepted </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${timeStamp}</td></tr>
-    <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>From </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${order_user}</td></tr>
-    </table>
-    <p><b>Order ID</b> - ${logid} </p>
-    <p><b>Bid ID</b> - ${original_logid} <b>Time Placed</b> - ${original_timeStamp} </p>
-  
-`;
-  }
-  //General receiving email for your order being accepted
-  else if (to == 'Accepted') {
-    return `<h3>Order Accepted!</h3>
-            <p>Hello ${current}! Your ${type} of ${produceName} was accepted by ${order_user}. Listed below is your accepted order </p>
-            <table style="border: 1px solid black; border-collapse: collapse;">
-            <tr><td style="border: 1px solid black; border-collapse: collapse; " ><b>Produce Name </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${produceName}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>Province </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${province}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>Quantity </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${quantity}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>Price </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${price}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>Time Accepted </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${timeStamp}</td></tr>
-            <tr><td style="border: 1px solid black; border-collapse: collapse;" ><b>From </b></td><td style="border: 1px solid black; border-collapse: collapse;" >${current}</td></tr>
-            </table>
-            <p><b>Order ID</b> - ${logid} </p>
-            <p><b>Offer ID</b> - ${original_logid} <b>Time Placed</b> - ${original_timeStamp} </p>
-          
-    `;
-  }
-}
 
 module.exports = router;
