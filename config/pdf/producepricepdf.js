19e16f84665d05b7426ec5ec06d3db07b57e6a06
DanielMAaron <danielaaron7@gmail.com>
2022-07-01 17:15:23 +0200

    added price date

--- config/pdf/producepricepdf.js
+++ config/pdf/producepricepdf.js
@@ -4,7 +4,14 @@ function producepricepdf(data) {
   let PDFStringdata = '';
   if (data.length) {
     for (var i = 0; i < data.length; i++) {
-      PDFStringdata = PDFStringdata + data[i].produce_name + ': R' + data[i].produce_price + '\n';
+      PDFStringdata =
+        PDFStringdata +
+        data[i].produce_name +
+        ': R' +
+        data[i].produce_price +
+        ' Updated: ' +
+        data[i].produce_date +
+        '\n';
     }
   } else {
     PDFStringdata = 'NO PRICE DATA AVAILABLE';
