685ca66832286cdd4cf300bf6000e7f082367af2
DanielMAaron <danielaaron7@gmail.com>
2022-07-01 14:39:26 +0200

    removed heading from data

--- config/pdf/producepricepdf.js
+++ config/pdf/producepricepdf.js
@@ -3,8 +3,6 @@ var moment = require('moment'); //datetime
 function producepricepdf(data) {
   let PDFStringdata = '';
   if (data.length) {
-    PDFStringdata =
-      'PRODUCE PRICE LIST AS OF ' + moment(new Date()).format('YYYY-MM-DD HH:mm:ss') + ' \n\n';
     for (var i = 0; i < data.length; i++) {
       PDFStringdata = PDFStringdata + data[i].produce_name + ': R' + data[i].produce_price + '\n';
     }
