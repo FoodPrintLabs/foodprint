f119530bfc17e868dc86526f112bfed3f3b48106
Julian Kanjere <julian.kanjere@jskdezign.com>
2022-07-03 00:34:43 +0200

    new db config using env variables

--- config/db/db_sequelise.js
+++ config/db/db_sequelise.js
@@ -1,5 +1,5 @@
 const env = process.env.NODE_ENV || 'development';
-const config = require('../../dbconfig')[env];
+const config = require('./dbconfig')[env];
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(config.database, config.username, config.password, {
