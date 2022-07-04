a2eca74194d02c2e8386ee0b2569bc0cf6843197
Julian Kanjere <julian.kanjere@jskdezign.com>
2022-07-05 00:48:31 +0200

    add staging environment

--- config/db/dbconfig.js
+++ config/db/dbconfig.js
@@ -16,12 +16,26 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
     logging: true,
   },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+  staging: {
+    use_env_variable: 'DATABASE_URL',
     dialect: process.env.DB_DIALECT,
     logging: false,
+    dialectOptions: {
+      ssl: {
+        /* <----- Add SSL option */ require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        /* <----- Add SSL option */ require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
