8cf15da5019d4c04eae6330daf39ecf4a85e18f5
Daniel Aaron <danielaaron7@gmail.com>
2022-08-25 18:10:53 +0200

    migration - add qrcode_url col

---
+++ migrations/20220825122057-add-column-storage-qrcode-url.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addColumn('foodprint_storage', 'qrcode_url', {
+      type: Sequelize.STRING,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('foodprint_storage', 'qrcode_url');
+  },
+};
